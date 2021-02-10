cc6c4a58ac8dcf1719099dfba58b1abe37d1ec04
theshashankk <78837537+theshashankk@users.noreply.github.com>
2021-02-10 00:32:02 -0800

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,6 +1,6 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
+# Copyright (C) 2019 the white spidy LLC.
 #
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# Licensed under the spidy Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
 """ Userbot initialization. """
